398a4666fc28203945c8ba6902f0659bd829c19b
cbalieiro <bmoliveira.camila@gmail.com>
2021-02-01 16:26:13 -0300

    JS/Adicionando provedores onde se encontram o filme

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,11 @@
 import data from '../../utils/config.js';
-import { searchAPI } from './API.js';
-import { templateAllCards } from '../../components/cardTemplate.js';
+import {
+  searchAPI
+} from './API.js';
+import {
+  templateAllCards,
+  templateProvider
+} from '../../components/cardTemplate.js';
 
 const dbObject = data.dataBase;
 const {
@@ -13,31 +18,40 @@ const {
   watchProviders,
 } = dbObject;
 
-let arrayMovieAndTv = []
+let arrayMovieAndTv = [];
+let arrayProviders = [];
 
-const write = 'homem aranha de volta ao lar';
+const write = 'homem aranha';
 const search = `&query=${write.replaceAll(' ', '%20')}`;
 
 let urlSearch = `${baseURL}${searchMulti}${apiKey}${language}${search}`;
 
 const getWatchProvider = (array) => {
   for (let index of array) {
-    let urlProvider = `${baseURL}${indice.media_type}/${indice.id}/${watchProviders}${apiKey}`
+    let urlProvider = `${baseURL}${index.media_type}/${index.id}/${watchProviders}${apiKey}`
     fetch(urlProvider)
-    .then(result=>result.json())
-    .then((searchReturn)=>{
-        console.log(searchReturn);
-        resultSearch.innerHTML = `
-        <img class="imagePoster" src="${baseImageURL}${imageSize}${searchReturn.results[0]['poster_path']}" width="150px">
-        `
-    })
+      .then(result => result.json())
+      .then((searchReturn) => {
+        let id = searchReturn.id;
+        let providers = searchReturn.results.BR;
+        arrayProviders.push({
+          id: id,
+          providers: providers
+        });
+        return templateProvider(arrayProviders);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
 };
 
 searchAPI(urlSearch)
   .then((data) => {
-  templateAllCards(data.results)
-  return arrayMovieAndTv = data.results;
+    templateAllCards(data.results)
+    return arrayMovieAndTv = data.results;
   })
-  .then((data) =>{
+  .then((data) => {
     return getWatchProvider(data)
+
   })
