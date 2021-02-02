386a61629b0bc96fdfc8bab0701770e68f30ca64
Kauana Maria Agostini <kauanaagostini@gmail.com>
2021-02-01 21:32:11 -0300

    criado função remoção de cards, e refatorado cards

--- src/js/index.js
+++ src/js/index.js
@@ -26,6 +26,7 @@ let arrayProviders = [];
 const inputUser = document.querySelector("#search-input");
 
 inputUser.addEventListener("keyup", () => {
+  clearDOM()
   let inputValue = inputUser.value.toLowerCase();
   let search = `&query=${inputValue.replaceAll(' ', '%20')}`;
   let urlSearch = `${baseURL}${searchMulti}${apiKey}${language}${search}`;
@@ -44,8 +45,7 @@ inputUser.addEventListener("keyup", () => {
 const getWatchProvider = (array) => {
   for (let index of array) {
     let urlProvider = `${baseURL}${index.media_type}/${index.id}/${watchProviders}${apiKey}`
-    fetch(urlProvider)
-      .then(result => result.json())
+    searchAPI(urlProvider)
       .then((searchReturn) => {
         let id = searchReturn.id;
         let providers = searchReturn.results.BR;
@@ -63,12 +63,21 @@ const getWatchProvider = (array) => {
 
 movie.addEventListener('click', (event) => {
   event.preventDefault();
+  clearDOM()
   templateButtons();
-  filterBy(movie, arrayMovieAndTv)
+  filterBy(movie, arrayMovieAndTv, arrayProviders)
 });
 
 tv.addEventListener('click', (event) => {
   event.preventDefault();
+  clearDOM()
   templateButtons();
-  filterBy(tv, arrayMovieAndTv)
+  filterBy(tv, arrayMovieAndTv, arrayProviders)
 });
+
+function clearDOM() {
+  let cards = document.querySelector('#page-main')
+  while (cards.firstChild) {
+    cards.removeChild(cards.firstChild);
+  }
+}
