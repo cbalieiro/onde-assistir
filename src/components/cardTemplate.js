d636d94e97c57673f245e5c150d52026e577b4ab
akemimeka <akemimeka@gmail.com>
2021-02-02 16:17:25 -0300

    Estiliza posters e ícones dos providers na pesquisa

--- src/components/cardTemplate.js
+++ src/components/cardTemplate.js
@@ -15,44 +15,46 @@ const {
 export const templateAllCards = (array) => {
   for (let index of array) {
     let cards = document.querySelector("#page-main");
-    let card = document.createElement("section");
-    card.classList.add('show-cards')
+    let card = document.createElement("div");
+    card.classList.add('card-title')
     cards.appendChild(card)
     card.innerHTML = `
-    <img class="imagePoster" src="${baseImageURL}${imageSize}${index.poster_path}" width="150px">
-    <div id="${index.id}"></div>
+      <img class="card-poster" src="${baseImageURL}${imageSize}${index.poster_path}">
+      <div id="${index.id}" class="provider-wrap"></div>
     `;
   }
 }
 
-export const templateProvider = (array) => { 
-  for (let index of array) { 
-    let providers = index.results; 
-    if(providers === undefined || providers.BR === undefined || providers.BR === null || providers.BR.flatrate === undefined) { 
-       if (document.getElementById(`${index.id}`)) { 
-         document.getElementById(`${index.id}`).innerHTML = 
-         ` <p> Não há serviço disponível no memento de Streaming Gratuito no Brasil para esse título</p>`; } } 
-         else {
-           if (document.getElementById(`${index.id}`)) { 
-             document.getElementById(`${index.id}`).innerHTML = 
-             ` <p> Stream </p>`; 
-             const arrayflatrate = index.results.BR.flatrate; 
-             arrayflatrate.forEach(element => {
-               let cardsProviders = document.getElementById(`${index.id}`);
-               let cardProvider =  document.createElement('div');
-               cardsProviders.appendChild(cardProvider);
-                cardProvider.innerHTML = 
-               `<img class="imgProvider" src="${baseImageURL}${imageSize}${element.logo_path}" width="40px"> `; 
-               element;
-             }); 
-            }
-    }    
-  } 
-} 
+export const templateProvider = (array) => {
+  for (let index of array) {
+    let providers = index.results;
+    if (providers === undefined || providers.BR === undefined || providers.BR === null || providers.BR.flatrate === undefined) {
+      if (document.getElementById(`${index.id}`)) {
+        document.getElementById(`${index.id}`).innerHTML =
+          `<p class="unavailable-title">Indisponível</p>`;
+      }
+    }
+    else {
+      if (document.getElementById(`${index.id}`)) {
+        document.getElementById(`${index.id}`).innerHTML = ``;
+        const arrayflatrate = index.results.BR.flatrate;
+        arrayflatrate.forEach(element => {
+          let cardsProviders = document.getElementById(`${index.id}`);
+          cardsProviders.classList.add('provider-wrap');
+          // let cardProvider = document.createElement('div');
+          // cardsProviders.appendChild(cardProvider);
+          cardsProviders.innerHTML =
+            `<img class="provider-img" src="${baseImageURL}${imageSize}${element.logo_path}"> `;
+          element;
+        });
+      }
+    }
+  }
+}
 
 
 export const filterBy = ((dataType, arraySearch, arrayProvider) => {
-  const filter = filterMethod(arraySearch,"==", dataType.id)
+  const filter = filterMethod(arraySearch, "==", dataType.id)
   templateAllCards(filter);
   templateProvider(arrayProvider);
 });
