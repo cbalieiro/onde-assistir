5c4220f031e7ff47882a6feefd8dcbf6e3583dea
BiancaSherika <bianca_sherika@hotmail.com>
2021-02-02 11:17:58 -0300

    adicionado funcionalidade modal ao footer e about us

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ import {
   templateAllCards,
   templateProvider
 } from '../components/cardTemplate.js';
+import { modalFooter } from '../components/modalfooter.js';
 
 const dbObject = data.dataBase;
 const {
@@ -24,11 +25,6 @@ let arryaMovieAndTvNotNull = [];
 let arrayProviders = [];
 
 const inputUser = document.querySelector("#search-input");
-
-<<<<<<< HEAD
-const footer = document.getElementById('footer');
-=======
->>>>>>> 43fd13e4098d5a981f600a180cf0e5951cc247be
 const movie = document.getElementById("movie");
 const tv = document.getElementById("tv");
 
@@ -99,58 +95,10 @@ function clearDOM() {
   }
 }
 
-// const modal = document.getElementById("myModal");
+const footerEvent = document.getElementById('footer');
 
-// const span = document.getElementsByClassName("close")[0];
+footerEvent.addEventListener('click', (event) => {
+  event.preventDefault();
+  modalFooter();
+})
 
-// footer.addEventListener('click', (event) => {
-//   event.preventDefault();
-//   console.log("oi")
-//   modal.style.dysplay;
-// })
-
-// span.addEventListener('click', (event) => {
-//   event.preventDefault();
-//   modal.style.display = "none";
-// })
-
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// } 
-
-// const showModal = (event) => {
-//   modal.classList.add('itemActive');
-//   let contentModal = modal.querySelector('.c-modal_content');
-//   contentModal.removeAttribute('id');
-//   // let elClick = event.target;
-//   // let cardClicked = elClick.closest('article.card');
-//   // let pokeNum = cardClicked.dataset.num;
-//   // let findCard = data.pokemon.find(pokemon => pokemon.num == pokeNum);
-//   // let boxType = `type-${findCard.type[0]}`;
-//   contentModal.setAttribute('id', boxType);
-//   contentModal.innerHTML = `
-//     <span class="close">&times;</span>
-//     <div class="c-modal-header"><img src="${findCard.img}" alt="">
-//       <h1>${findCard.name}</h1>
-//       <h2><strong>Type:</strong> ${typeWithSpace(findCard.type)}</h2>
-//     </div>
-//     <div class="c-modal-container">
-//       <p><strong>Height:</strong> ${findCard.size.height}</p>
-//       <p><strong>Weight:</strong> ${findCard.size.weight}</p>
-//       <p><strong>About:</strong> ${findCard.about}</p>
-//     </div>
-//     <div class="c-modal-footer">
-//       <p><strong>Quick Move:</strong> ${findCard['quick-move'][0].name}</p>
-//       <p><strong>Special-Attack:</strong> ${findCard['special-attack'][0].name}</p>
-//       <p>${findCard.name.toUpperCase()} has base-attack greater than ${getBaseAttackPercentage(data.pokemon, findCard)}% of Pokémons.</p>
-//     </div>`;
-//   let btClose = modal.querySelector('.close');
-//   btClose.addEventListener('click', hiddenModal);
-// }
-
-// const hiddenModal = () => {
-//   modal.classList.remove('itemActive');
-// }
-};
