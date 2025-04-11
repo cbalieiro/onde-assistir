c29c340ca4205880438b4b155035eb412bbad8ae
Camila Balieiro Mello Oliveira <bmoliveira.camila@gmail.com>
2025-04-11 16:22:38 -0300

    Add error handling for specific HTTP status codes in fetchDataFromAPI

--- src/js/fetchDataFromAPI.js
+++ src/js/fetchDataFromAPI.js
@@ -6,6 +6,9 @@ export const fetchDataFromAPI = async url => {
     if (!response.ok) {
       throw new Error(response.statusText);
     }
+    if (erros[response.status]) {
+      throw new Error(erros[response.status]);
+    }
     const data = await response.json();
     if (!isValidTheMovieDBResponse(data)) {
       throw new Error("Formato inesperado da resposta da API TheMovieDB: " + JSON.stringify(data));
@@ -25,4 +28,14 @@ function isValidTheMovieDBResponse(data) {
     typeof data.total_pages === 'number' &&
     typeof data.total_results === 'number') || (typeof data.id === 'number')
   );
+}
+
+const erros = { 
+  400: "Requisição inválida. Verifique os parâmetros.",
+  401: "Acesso não autorizado. Verifique sua chave de API.",
+  404: "Recurso não encontrado. Verifique a URL.",
+  500: "Erro interno do servidor. Tente novamente mais tarde.",
+  503: "Serviço temporariamente indisponível. Tente novamente mais tarde.",
+  504: "Tempo limite de solicitação. Tente novamente mais tarde.",
+  default: "Erro desconhecido. Tente novamente mais tarde."
 }
